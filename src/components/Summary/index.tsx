8b48a5a5c0b5844049ee9daa3cad06d91870d845
Diego Coelho <diegocoelhoaprova@gmail.com>
2026-02-02 21:51:48 -0300

    feat: add Summary component and integrate into Transactions page

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,42 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
+
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <div>
+          <header>
+            <span>Entradas</span>
+            <ArrowCircleUp size={32} color="#00b37e" />
+          </header>
+
+          <strong> R$ 17.400,00</strong>
+        </div>
+      </SummaryCard>
+
+      <SummaryCard>
+        <div>
+          <header>
+            <span>Saídas</span>
+            <ArrowCircleDown size={32} color="#f75a68" />
+          </header>
+
+          <strong> R$ 1.259,00</strong>
+        </div>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <div>
+          <header>
+            <span>Total</span>
+            <CurrencyDollar size={32} color="#fff" />
+          </header>
+
+          <strong> R$ 17.400,00</strong>
+        </div>
+      </SummaryCard>
+    </SummaryContainer >
+  )
+}
