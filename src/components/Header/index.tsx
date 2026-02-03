d19d27b26fa522c6a4544b4e5f8c62a1c889fb90
Diego Coelho <diegocoelhoaprova@gmail.com>
2026-02-02 23:19:03 -0300

    feat(modal): create NewTransactionModal component with styled elements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from '../../assets/Logo.svg'
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
 	return (
@@ -16,15 +17,7 @@ export function Header() {
 					</Dialog.DialogTrigger>
 
 
-					<Dialog.Portal>
-						<Dialog.Overlay />
-
-						<Dialog.Content>
-							<Dialog.Title>Nova transação</Dialog.Title>
-
-							<Dialog.Close />
-						</Dialog.Content>
-					</Dialog.Portal>
+					<NewTransactionModal />
 				</Dialog.Root>
 
 			</HeaderContent>
